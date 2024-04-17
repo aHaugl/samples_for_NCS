5bd174fea40c06d5c5087206010da39ae6006523
aHaugl <andreas.haugland1@gmail.com>
2024-04-17 11:09:07 +0200

    cleanup

--- DFU/smp_ble_feat_spi/build_1/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "C:/Nordic/SDKs/ncs/my_projects/samples_for_NCS/DFU/smp_ble_feat_spi/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "C:/Nordic/SDKs/ncs/my_projects/samples_for_NCS/DFU/smp_ble_feat_spi/app.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/Nordic/SDKs/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
-set(mcuboot_DTC_OVERLAY_FILE "C:/Nordic/SDKs/ncs/v2.6.0/nrf/modules/mcuboot/usb.overlay;C:/Nordic/SDKs/ncs/my_projects/samples_for_NCS/DFU/smp_ble_feat_spi/child_image/mcuboot.overlay" CACHE INTERNAL "NCS child image controlled")
